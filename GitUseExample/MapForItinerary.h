88a64d2e4cbaf55e47a541537c1f4d7d7dc4af21
Stephen Sproul <sesproul@gmail.com>
2013-06-02 18:24:32 -0400

    Started work on new map feature, not quite done yet.

---
+++ GitUseExample/MapForItinerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.h
+//  GitUseExample
+//
+//  Created by sesproul on 6/2/13.
+//  Copyright (c) 2013 SDS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForItinerary : NSObject
+
+@end
---
+++ GitUseExample/MapForItinerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.m
+//  GitUseExample
+//
+//  Created by sesproul on 6/2/13.
+//  Copyright (c) 2013 SDS. All rights reserved.
+//
+
+#import "MapForItinerary.h"
+
+@implementation MapForItinerary
+
+@end
